c37286336f32a30bbb3951f375ed9b18dfb940f8
Jacek Kosinski <kashiash@gmail.com>
2023-02-26 00:08:31 +0100

    .

--- UltimatePortfolio/SidebarView.swift
+++ UltimatePortfolio/SidebarView.swift
@@ -8,13 +8,33 @@
 import SwiftUI
 
 struct SidebarView: View {
+    @EnvironmentObject var dataController: DataController
+    let smartFilters: [Filter] = [.all, .recent]
+    
+    @FetchRequest (sortDescriptors: [SortDescriptor(\.name)]) var tags: FetchedResults<Tag>
+    
+    var tagFilters: [Filter] {
+        tags.map { tag in
+            Filter(id: tag.id ?? UUID(), name: tag.name ?? "No name", icon: "tag", tag: tag)
+        }
+    }
     var body: some View {
-        Text("Sidebar")
+        List(selection: $dataController.selectedFilter){
+            Section("Smart Filters") {
+                ForEach(smartFilters){ filter in
+                    NavigationLink(value:filter){
+                        Label(filter.name,systemImage: filter.icon)
+                    }
+                }
+            }
+        }
+        
     }
 }
 
 struct SidebarView_Previews: PreviewProvider {
     static var previews: some View {
         SidebarView()
+            .environmentObject(DataController.preview)
     }
 }
