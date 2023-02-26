085e5a695e8bda65d89d17200ba0dcdec1b84029
Jacek Kosinski <kashiash@gmail.com>
2023-02-26 10:34:46 +0100

    fixes

--- UltimatePortfolio/ContentView.swift
+++ UltimatePortfolio/ContentView.swift
@@ -8,6 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @EnvironmentObject var dataController: DataController
+    
+    var issues: [Issue] {
+        let filter = dataController.selectedFilter ?? .all
+        var allIssues: [Issue]
+
+        if let tag = filter.tag {
+            allIssues = tag.issues?.allObjects as? [Issue] ?? []()
+        } else {
+            let request = Issue.fetchRequest()
+            request.predicate = NSPredicate(format: "modificationDate > %@", filter.minModificationDate as NSDate)
+            allIssues = (try? dataController.container.viewContext.fetch(request)) ?? []
+        }
+
+        return allIssues.sorted()
+    }
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
--- UltimatePortfolio/SidebarView.swift
+++ UltimatePortfolio/SidebarView.swift
@@ -13,11 +13,14 @@ struct SidebarView: View {
     
     @FetchRequest (sortDescriptors: [SortDescriptor(\.name)]) var tags: FetchedResults<Tag>
     
+    
     var tagFilters: [Filter] {
         tags.map { tag in
-            Filter(id: tag.id ?? UUID(), name: tag.name ?? "No name", icon: "tag", tag: tag)
+            Filter(id: tag.tagID, name: tag.tagName, icon: "tag", tag: tag)
         }
     }
+    
+
     var body: some View {
         List(selection: $dataController.selectedFilter){
             Section("Smart Filters") {
@@ -31,6 +34,7 @@ struct SidebarView: View {
                 ForEach(tagFilters) { filter in
                     NavigationLink(value: filter){
                         Label(filter.name, systemImage: filter.icon)
+                            .badge(filter.tag?.tagActiveIssues.count ?? 0)
                     }
                 }
             }
