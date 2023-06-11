c8e887ebe811172b4a351e4b4279b4c5e047fa2f
Jacek Kosiński <kashiash@gmail.com>
2023-06-11 14:29:04 +0200

    another grouping style

--- UltimatePortfolio/Filters/SidebarView.swift
+++ UltimatePortfolio/Filters/SidebarView.swift
@@ -31,25 +31,7 @@ struct SidebarView: View {
 
             Section("Tags") {
                 ForEach(tagFilters) { filter in
-                    NavigationLink(value: filter) {
-                        Label(filter.name, systemImage: filter.icon)
-                            .badge(filter.activeIssuesCount)
-                            .contextMenu {
-                                Button {
-                                    rename(filter)
-                                } label: {
-                                    Label("Rename", systemImage: "pencil")
-                                }
-                                Button(role: .destructive) {
-                                    delete(filter)
-                                } label: {
-                                    Label("Delete", systemImage: "trash")
-                                }
-                            }
-                            .accessibilityElement()
-                            .accessibilityLabel(filter.name)
-                            .accessibilityHint("^[\(filter.activeIssuesCount) issue](inflect: true)")
-                    }
+                    UserFilterRow(filter: filter, rename: rename, delete: delete)
                 }
                 .onDelete(perform: delete)
             }
